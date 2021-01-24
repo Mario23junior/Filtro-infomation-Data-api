a6ac7e7c1d4e9b6305bef70c5bee775390cfbc95
Mario23junior <mariojunior3251@gmail.com>
2021-01-24 18:01:57 -0300

    Criando camada de filtro relacional DTO das entidades

---
+++ src/main/java/com/project/filterData/DTO/UserLocationDTO.java
@@ -0,0 +1,43 @@
+package com.project.filterData.DTO;
+
+public class UserLocationDTO {
+   
+	private Integer id;
+	private String username;
+	private double lat;
+	private double lng;
+	private String place;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public double getLat() {
+		return lat;
+	}
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+	public double getLng() {
+		return lng;
+	}
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	
+	
+}
