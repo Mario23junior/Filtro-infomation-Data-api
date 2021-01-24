610480d9e6b46415d09815055413e5406e8f0177
Mario23junior <mariojunior3251@gmail.com>
2021-01-24 16:12:11 -0300

    Iniciando projeto de filtragem de dados

---
+++ src/test/java/com/project/filterData/FilterDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.filterData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
