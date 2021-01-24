a6abcdfc5f5c63f4c04c7cd62f9d805d966bfaeb
Mario23junior <mariojunior3251@gmail.com>
2021-01-24 18:17:04 -0300

    Criando camada service DTO filter

---
+++ src/main/java/com/project/filterData/Service/UserLocationDTO.java
@@ -0,0 +1,13 @@
+package com.project.filterData.Service;
+
+import com.project.filterData.Repository.UserRepository;
+
+public class UserLocationDTO {
+
+	private UserRepository userRepository;
+	
+	public UserLocationDTO(UserRepository userRepository) {
+		this.userRepository = userRepository;
+ 	}
+	
+}
