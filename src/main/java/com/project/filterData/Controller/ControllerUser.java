a3356213814c0a0c904a9fbee4aee5678485c115
Mario23junior <mariojunior3251@gmail.com>
2021-01-24 23:06:53 -0300

    Criando controller de User

--- src/main/java/com/project/filterData/Controller/ControllerUser.java
+++ src/main/java/com/project/filterData/Controller/ControllerUser.java
@@ -1,5 +1,29 @@
 package com.project.filterData.Controller;
 
-public class ControllerUser {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.filterData.DTO.UserLocationDTO;
+import com.project.filterData.Service.MapInformation;
+
+@RestController
+@RequestMapping("/user/information")
+public class ControllerUser {
+     
+	private MapInformation mapInformation;
+	
+	public ControllerUser(MapInformation mapInformation) {
+		 this.mapInformation = mapInformation;
+ 	}
+	
+	
+	@GetMapping("/map")
+	public List<UserLocationDTO> ListInformation() {
+		List <UserLocationDTO> userLocation = mapInformation.getAllUserLocation();
+		return userLocation;
+	}
+	
 }
--- src/main/java/com/project/filterData/Service/MapInformation.java
+++ src/main/java/com/project/filterData/Service/MapInformation.java
@@ -3,11 +3,14 @@ package com.project.filterData.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
 import com.project.filterData.DTO.UserLocationDTO;
 import com.project.filterData.Model.Location;
 import com.project.filterData.Model.User;
 import com.project.filterData.Repository.UserRepository;
 
+@Service
 public class MapInformation {
 
 	private UserRepository userRepository;
